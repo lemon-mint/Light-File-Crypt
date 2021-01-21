6fd12c5fb21098c565b37609f37bf8b816b27937
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-01-22 08:51:48 +0900

    Encryption fixed

--- main.go
+++ main.go
@@ -18,23 +18,36 @@ func main() {
 		return
 	}
 	bc, _ := golea.NewCipher([]byte("mysuperstrongencryptionkey000000"))
-	f, err := os.Open("test.txt")
+	f, err := os.Open("1000MB.bin")
 	if err != nil {
 		return
 	}
+	defer f.Sync()
 	encryptFileCBC(f, output, bc, make([]byte, 16))
 }
 
-func encryptFileCBC(input *os.File, output *os.File, bc cipher.Block, iv []byte) error {
+func encryptFileCBC(input io.Reader, output io.Writer, bc cipher.Block, iv []byte) error {
 	EncBuf := make([]byte, 16)
 	EncryptedBuf := make([]byte, 16)
+	r := bufio.NewReaderSize(input, 1024*1024*10)
 	w := bufio.NewWriterSize(output, 1024*1024*10)
 	defer w.Flush()
-	defer output.Sync()
 
 	w.Write(iv)
 	for {
-		n, err := input.Read(EncBuf)
+		n, err := r.Read(EncBuf)
+		if err == io.EOF {
+			EncBuf = make([]byte, 16)
+			io.ReadFull(rand.Reader, EncBuf)
+			EncBuf[15] = 16
+			for i := range EncBuf {
+				EncBuf[i] = EncBuf[i] ^ iv[i]
+			}
+			bc.Encrypt(EncryptedBuf, EncBuf)
+			copy(iv, EncryptedBuf)
+			w.Write(EncryptedBuf)
+			break
+		}
 		if err != nil {
 			return err
 		}
@@ -51,8 +64,8 @@ func encryptFileCBC(input *os.File, output *os.File, bc cipher.Block, iv []byte)
 			copy(tmp, EncBuf)
 			EncBuf = make([]byte, 16)
 			copy(EncBuf, tmp)
+			io.ReadFull(rand.Reader, randbuf)
 			for i := n; i < 16; i++ {
-				io.ReadFull(rand.Reader, randbuf)
 				EncBuf[i] = randbuf[i]
 			}
 			EncBuf[15] = byte(16 - n)
